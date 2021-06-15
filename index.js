88fd0bacff26f0983e26f81544e2ce27761d262e
Kamogelo Mashike <marykamogelo387@gmail.com>
2021-06-15 13:43:55 +0200

    Added Other Operators to the simple calc

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ function add(x , y){
     let sum  = x + y;
     console.log (sum);
 }
+function multiply(num1,num2){
+    console.log(num1*num2);
+}
+function divide(d,v){
+    console.log(d/v);
+}
+function sub(s,l) {
+    console.log(s-1);
+}
 
 add(5 , 3);
+multiply(2.2,2);
+divide(8,2);
+sub(673,6);
